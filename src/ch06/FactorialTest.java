5725a468a7ad2cdd4ce9488944a5b811313ea9fa
SHIM <weasd33@naver.com>
2022-10-19 19:24:42 +0900

    재귀호출 - 유효성 검사 추가

--- src/ch06/FactorialTest.java
+++ src/ch06/FactorialTest.java
@@ -2,12 +2,13 @@ package ch06;
 
 class FactorialTest {
 	public static void main(String[] args) {
-		int result = factorial(4);
+		int result = factorial(5);
 		
 		System.out.println(result);
 	}
 
 	static int factorial(int n) {
+		if(n <= 0 || n > 12) return -1; // 유효성 검사(StackOverFlow Error 방지)
 		if(n == 1) return 1;
 		
 		return n * factorial(n-1); // 다시 메서드를 호출한다.
