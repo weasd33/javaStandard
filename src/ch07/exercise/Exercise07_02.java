ad5d3fd3ad983dcd36704843481b0cd4bd706210
SHIM <weasd33@naver.com>
2022-11-26 18:32:44 +0900

    [ch07. 연습문제] - MyTv2클래스 이전 채널 메서드 추가

--- src/ch07/exercise/Exercise07_02.java
+++ src/ch07/exercise/Exercise07_02.java
@@ -1,14 +1,27 @@
 package ch07.exercise;
 
 /*
+	- Exercise07_02 -
 	MyTv2클래스의 멤버변수 isPowerOn, channel, volume을 클래스 외부에서 접근할 수 없도록 제어자를 붙이고
 	대신 이 멤버변수들의 값을 어디서나 읽고 변경할 수 있도록 setter와 getter메서드를 추가하라.
+	
+	------------------------------------------------------------------------------------
+	
+	- Exercise07_02-2 -
+	MyTv2클래스에 이전 채널(previous channel)로 이동하는 기능의 메서드를 추가해서 실행결과와 같은 결과를 얻도록 하시오.
+	[Hint] 이전 채널의 값을 저장할 멤버변수를 정의하라.
+	
+	메서드명 : gotoPrevChannel
+	기   능 : 현재 채널을 이전 채널로 변경한다.
+	반환타입 : 없음
+	매개변수 : 없음
 */
 
 class MyTv2 {
 	private boolean isPowerOn;
 	private int channel;
 	private int volume;
+	private int prevChannel; // 이전 채널
 	
 	final int MAX_VOLUME = 100;
 	final int MIN_VOLUME = 0;
@@ -16,23 +29,23 @@ class MyTv2 {
 	final int MIN_CHANNEL = 1;
 	
 	// (1) 알맞은 코드를 넣어 완성하시오.
-	public boolean isPowerOn() {
-		return isPowerOn;
-	}
-	public void setPowerOn(boolean isPowerOn) {
-		this.isPowerOn = isPowerOn;
-	}
 	public int getChannel() {
 		return channel;
 	}
 	public void setChannel(int channel) {
-		this.channel = channel;
+		this.prevChannel = this.channel; // 현재 채널을 이전 채널에 저장
+		this.channel = channel; // 매개변수로 넘어 온 채널 값을 현재 채널로 설정
 	}
 	public int getVolume() {
 		return volume;
 	}
 	public void setVolume(int volume) {
 		this.volume = volume;
+	}
+	
+	// (2) gotoPrevChannel메서드 추가
+	void gotoPrevChannel() {
+		setChannel(this.prevChannel); // 현재 채널을 이전 채널로 변경
 	}
 }
 
@@ -42,7 +55,11 @@ public class Exercise07_02 {
 		
 		t.setChannel(10);
 		System.out.println("CH:"  + t.getChannel());
-		t.setVolume(20);
-		System.out.println("VOL:" + t.getVolume());
+		t.setChannel(20);
+		System.out.println("CH:"  + t.getChannel());
+		t.gotoPrevChannel();
+		System.out.println("CH:" + t.getChannel());
+		t.gotoPrevChannel();
+		System.out.println("CH:" + t.getChannel());
 	}
 }
